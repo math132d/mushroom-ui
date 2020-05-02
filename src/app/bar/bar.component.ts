2ec2ef6e76a24aae5362565e3acb97de1678f2af
math132d <majohha@gmail.com>
2020-05-02 16:22:39 +0200

    Added top bar

--- src/app/bar/bar.component.ts
+++ src/app/bar/bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-bar',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bar.component.css']
 })
 export class BarComponent implements OnInit {
+  search: String = '';
+  inFocus: Boolean = false;
+  suggestions: any = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  onChange(search: String): void {
+    this.search = search;
+    this.getSuggestions();
+  }
+
+  onFocusChange(val: Boolean) {
+    if(val) {
+      this.inFocus = val;
+    }else{
+      window.setTimeout(() => {
+        this.inFocus = val;
+      }, 100);
+    }
+  }
+
+  getSuggestions(){
+    this.http.get<any>(`http://thestaticcow.dk:30031/search/${this.search}`).subscribe(data => {
+      this.suggestions = data;
+    })
+  }
 }
